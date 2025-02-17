a4cfa8a3d1560eb29b747f4c6bc585e23616a097
mohamed-nousad <mohamed.nousad@amuryx.com>
2025-02-17 07:38:36 +0530

    last updted

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://district-transfer-management-system-te19.vercel.app",
     credentials: true,
   })
 );
