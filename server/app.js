fc0de3f6e7dbd066dd375b394a93fc7e2d30196b
mohamed-nousad <mohamed.nousad@amuryx.com>
2025-02-22 17:54:59 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://district-transfer-management-system-jado.vercel.app",
+    origin: "https://dtms.app",
     credentials: true,
   })
 );
